3ca0e9360e98a4983846b929eaa1df539b5ea784
Rishabh Srivastava <rishabh.srivastava0205@gmail.com>
2023-11-25 22:44:03 +0530

    modified code commit to repository

--- client/src/axiosBack.jsx
+++ client/src/axiosBack.jsx
@@ -1,16 +1,19 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
+// global base url for client side
 const axiosBackend = axios.create({
     baseURL: "http://localhost:9001/api/admin"
-})
+});
 
+// handle request from the client side
 axiosBackend.interceptors.request.use((config) => {
     const jwtToken = Cookies.get('ADMIN_ACCESS_TOKEN');
     config.headers.Authorization = `Bearer ${jwtToken}`;
     return config;
 });
 
+// handle response, getting from the server side
 axiosBackend.interceptors.response.use((response) => {
     return response;
 }, (error) => {
--- client/src/axiosFront.jsx
+++ client/src/axiosFront.jsx
@@ -1,16 +1,19 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
+// global base url for client side
 const axiosFrontend = axios.create({
     baseURL: "http://localhost:9001/api"
-})
+});
 
+// handle request from the client side
 axiosFrontend.interceptors.request.use((config) => {
     const jwtToken = Cookies.get('WEB_ACCESS_TOKEN');
     config.headers.Authorization = `Bearer ${jwtToken}`;
     return config;
 });
 
+// handle response, getting from the server side
 axiosFrontend.interceptors.response.use((response) => {
     return response;
 }, (error) => {
